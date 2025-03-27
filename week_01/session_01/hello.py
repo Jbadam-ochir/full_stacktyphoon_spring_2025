68bd13584e8b3392bb0b6232a307d8ffeaca46f9
Badam-ochir <jbadmaa19@gmail.com>
2025-03-27 17:53:42 +0800

    add: session_02

---
+++ week_01/session_01/hello.py
@@ -0,0 +1,1 @@
+print ('hellofullstack developer')
---
+++ week_01/session_02/introduce.py
@@ -0,0 +1,9 @@
+# introduce myself -- this comment 
+# introduce yourself in three lines
+# for example: your first name, last name, age, gender
+#,occupation and hobbies
+print("My name is badam ochir ")
+print("i'm20 years old")
+print("i'm man") 
+print("i'm i code academy student") 
+print(" i like basksetball")
